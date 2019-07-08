29fb46a9e98d33842cb940a26acca84d35738810
Pratiksha <Pratiksha.mokashi@neosofttech.com>
2019-07-08 16:26:45 +0530

    auto slider done

--- app/src/main/java/com/example/neostoreapp/ui/home/HomeActivity.kt
+++ app/src/main/java/com/example/neostoreapp/ui/home/HomeActivity.kt
@@ -2,7 +2,9 @@ package com.example.neostoreapp.ui.home
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import android.support.v4.view.ViewPager
+import android.util.Log
 import android.view.View
 import com.example.neostoreapp.R
 import com.example.neostoreapp.ui.base.BaseActivity
@@ -12,8 +14,11 @@ import com.example.neostoreapp.ui.login.LoginContract
 import com.example.neostoreapp.ui.login.LoginPresenter
 import com.example.neostoreapp.ui.login.LoginResponse
 import com.example.neostoreapp.ui.productlisting.ProductActivity
+import com.viewpagerindicator.CirclePageIndicator
 import kotlinx.android.synthetic.main.activity_home.*
 import kotlinx.android.synthetic.main.toolbar.*
+import java.util.*
+import kotlin.collections.ArrayList
 
 class HomeActivity: BaseActivity(), LoginContract.LoginView  {
     override var layout= R.layout.activity_home
@@ -21,13 +26,16 @@ class HomeActivity: BaseActivity(), LoginContract.LoginView  {
     override val getPresenter: BasePresenter
      get() = presnter
 
+    private var imageModelArrayList:ArrayList<ImageModel>?=null
+    private val myimagelist= intArrayOf(R.drawable.slider_img1,R.drawable.slider_img2,R.drawable.slider_img3,R.drawable.slider_img4)
+
     internal lateinit var viewPager: ViewPager
       //  var search = findViewById(R.id.search_img) as ImageView
     override fun init() {
       ab_back_white.visibility=View.GONE
-        viewPager=findViewById<View>(R.id.viewPager) as ViewPager
-        val adapter =ViewPageAdapter(this)
-        viewPager.adapter=adapter
+          imageModelArrayList= ArrayList()
+          imageModelArrayList=populateList()
+          showPager()
 
           tableicn.setOnClickListener {
               var bundle=Bundle()
@@ -36,6 +44,7 @@ class HomeActivity: BaseActivity(), LoginContract.LoginView  {
               intent.putExtras(bundle)
               startActivity(intent)
           }
+
 
 /*
     menu_img.setOnClickListener(){
@@ -49,6 +58,66 @@ class HomeActivity: BaseActivity(), LoginContract.LoginView  {
 
 
     }
+    private fun populateList():ArrayList<ImageModel> {
+        Log.d("Tag","populate method")
+        val list=ArrayList<ImageModel>()
+        for (i in 0..myimagelist.size-1){
+            var imageModel=ImageModel()
+            imageModel.setImage_drawable(myimagelist[i])
+            list.add(imageModel)
+        }
+        return list
+    }
+
+    private fun showPager()
+    {
+        var mPager=findViewById(R.id.viewPager) as ViewPager
+        mPager.adapter=SlidingImageAdapter(this,this.imageModelArrayList!!)
+        val indicator=findViewById(R.id.indicator) as CirclePageIndicator
+        indicator.setViewPager(mPager)
+       // val density=resources.displayMetrics.density
+        indicator.radius
+        NUM_PAGES = imageModelArrayList!!.size
+
+        val handler= Handler()
+        val update= Runnable {
+            if (currentPage==NUM_PAGES){
+                currentPage=0
+            }
+            mPager.setCurrentItem(currentPage++,true)
+        }
+        val swipeTimer= Timer()
+        swipeTimer.schedule(object: TimerTask()
+        {
+            override fun run() {
+                handler.post(update)
+            }
+        },1000,1000)
+
+
+        indicator.setOnPageChangeListener(object :ViewPager.OnPageChangeListener
+        {
+            override fun onPageScrollStateChanged(p0: Int) {
+            }
+
+            override fun onPageScrolled(p0: Int, p1: Float, p2: Int) {
+            }
+
+            override fun onPageSelected(p0: Int) {
+                currentPage = p0
+            }
+        })
+    }
+    companion object {
+
+        private var mPager: ViewPager? = null
+        private var currentPage = 0
+        private var NUM_PAGES = 0
+    }
+
+
+
+
 
 
     override fun loginFailure(errorMsg: String) {
---
+++ app/src/main/java/com/example/neostoreapp/ui/home/ImageModel.kt
@@ -0,0 +1,12 @@
+package com.example.neostoreapp.ui.home
+
+class ImageModel {
+    private var image_drawable:Int=0
+    fun getImage_drawable():Int{
+        return image_drawable
+    }
+    fun setImage_drawable(image_drawable:Int){
+        this.image_drawable=image_drawable
+    }
+
+}
